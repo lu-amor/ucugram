20fbb4e11517964cd58fcf26d6cdb2eb7347e33e
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2024-09-29 20:48:05 -0300

    add logic to toggle between login and create account modals

--- ucugram/src/pages/loginPage/loginPage.jsx
+++ ucugram/src/pages/loginPage/loginPage.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./loginPage.module.css";
 import LoginForm from "../../components/loginForm/loginForm.jsx";
+import CreateAccountForm from "../../components/createAccountForm/createAccountForm.jsx";
 
 const LoginPage = () => {
+  const [showCreateAccountForm, setShowCreateAccountForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleCreateAccountBtn = () => {
+    setShowCreateAccountForm(true);
+    setShowLoginForm(false);
+  };
+
+  const handleGoLoginModalBtn = () => {
+    setShowCreateAccountForm(false);
+    setShowLoginForm(true);
+  }
+
   return (
     <div className="loginPage">
-      <LoginForm />
+      {showLoginForm && <LoginForm createAccount={handleCreateAccountBtn}/>}
+      {showCreateAccountForm && <CreateAccountForm closeModal={handleGoLoginModalBtn}/>}
     </div>
   );
 };
