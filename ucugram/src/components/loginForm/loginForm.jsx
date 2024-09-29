d4854bbf9547d4c77c399f6bc43c614261f3b69c
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2024-09-29 19:48:46 -0300

    add logo into loginForm

--- ucugram/src/components/loginForm/loginForm.jsx
+++ ucugram/src/components/loginForm/loginForm.jsx
@@ -5,13 +5,31 @@ function LoginForm() {
   const emailRef = useRef("");
   const passwordRef = useRef("");
 
+  const handleCreateAccountBtn = () => {
+    // logica para crear cuenta here
+  };
+
+  const handleLoginBtn = () => {
+    const newEmail = emailRef.current.value;
+    const newPassword = passwordRef.current.value;
+
+    // aca se hace algo más
+  };
+
   return (
     <div className={classes.modal}>
       <div className={classes.modalContent}>
         <div className={classes.modalContainer}>
-            <div className={classes.logoContainer}>
-                <img src="/ucugram-text-sinFondo.png" alt="logo" className={classes.isologo} />
-            </div>
+          <div className={classes.logoContainer}>
+            <img src="/ucugram-logo.png" className={classes.logo} />
+          </div>
+          <div className={classes.logoContainer}>
+            <img
+              src="/ucugram-text-sinFondo.png"
+              alt="logo"
+              className={classes.isologo}
+            />
+          </div>
           <div className={classes.field}>
             <label className="label">email</label>
             <input
@@ -31,7 +49,20 @@ function LoginForm() {
             ></input>
           </div>
           <div className={classes.buttonContainer}>
-            <button className={`button ${classes.loginButton}`}>Login</button>
+            <button
+              className={`button ${classes.loginButton}`}
+              onClick={handleLoginBtn}
+            >
+              Login
+            </button>
+          </div>
+          <div className={classes.buttonContainer}>
+            <button
+              style={{ font: "12px arial", marginBottom: "10px" }}
+              onClick={handleCreateAccountBtn}
+            >
+              Create account <strong>here</strong>
+            </button>
           </div>
         </div>
       </div>
