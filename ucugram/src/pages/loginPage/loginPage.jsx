dcec4d520451e80a7bdeb8c4e641cb0c44326f9e
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2024-09-29 19:22:23 -0300

    add loginForm component into loginPage

--- ucugram/src/pages/loginPage/loginPage.jsx
+++ ucugram/src/pages/loginPage/loginPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import classes from './loginPage.module.css';
+import classes from "./loginPage.module.css";
+import LoginForm from "../../components/loginForm/loginForm.jsx";
 
 const LoginPage = () => {
-    return (
-        <div className="loginPage">
-
-        </div>
-    );
+  return (
+    <div className="loginPage">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
