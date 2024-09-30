7ab179492b180dda262f98fd525232a45bddc1bd
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2024-09-30 09:41:12 -0300

    add profile page for user with basic layout

--- ucugram/src/App.jsx
+++ ucugram/src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import FeedPage from './pages/feedPage/feedPage'
-import MyProfile from './pages/myProfile/myProfile'
-import FriendProfile from './pages/friendProfile/friendProfile'
-import SideNavBar from './components/sideNavBar/sideNavBar'
-
-import './App.css'
+import React from "react";
+import FeedPage from "./pages/feedPage/feedPage";
+import MyProfile from "./pages/myProfile/myProfile";
+import FriendProfile from "./pages/friendProfile/friendProfile";
+import SideNavBar from "./components/sideNavBar/sideNavBar";
+// import LoginPage from './pages/loginPage/loginPage'
+import Avatar from "./components/avatar/avatar";
 
 function App() {
+  const user = {
+    profilePhoto: "/ucugram-logo.png",
+    name: "nombre_ususario",
+    posts: 11, // después esto va a ser una lista por ej. 
+    friends: 17 // lista o dict con usuarios amigos
+  };
   return (
     <>
-      <FeedPage />
+      <MyProfile
+        user={user}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ucugram/src/pages/myProfile/myProfile.jsx
+++ ucugram/src/pages/myProfile/myProfile.jsx
@@ -1,13 +1,48 @@
 import React from "react";
+import classes from "./MyProfile.module.css";
+import Avatar from "../../components/avatar/avatar";
 import SideNavBar from "../../components/sideNavBar/sideNavBar";
 
-const MyProfile = () => {
-    return (
-        <div className="columns">
-            <SideNavBar />
-            <div className="column is-10">resto de la página</div>
+function MyProfile({ user }) {
+  return (
+    <div className="columns">
+      <SideNavBar />
+      <div className="column is-10">
+        <div className={classes.profileContainer}>
+          <div className={classes.header}>
+            <div className={classes.avatar}>
+              <Avatar user={user} />
+            </div>
+            <div className={classes.profileInformation}>
+              <p style={{ font: "25px Segoe UI", width: "50%" }}>
+                <strong>{user.name}</strong>
+              </p>
+              <button className={`button ${classes.profileButton}`}>
+                Edit profile
+              </button>
+              <div className={classes.accountInformation}>
+                <p style={{ gridColumn: 1, gridRow: 1 }}>
+                  <strong>{user.posts}</strong> posts
+                </p>
+                <p style={{ gridColumn: 2, gridRow: 1 }}>
+                  <strong>{user.friends}</strong> friends
+                </p>
+                <div style={{ gridColumn: "1 / 4", gridRow: 2, marginTop:"15px" }}>
+                  <p>
+                    mini descripción del perfil 😊
+                  </p>
+                </div>
+              </div>
+              <div></div>
+            </div>
+          </div>
+          <div>
+            aca va el resto del perfil
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
 
 export default MyProfile;
