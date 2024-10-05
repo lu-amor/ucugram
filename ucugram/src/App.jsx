ea34934da7608adcdb3cc66a7d0479a55cde99d7
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2024-10-05 09:53:19 -0300

    add profile photo

--- ucugram/src/App.jsx
+++ ucugram/src/App.jsx
@@ -11,12 +11,21 @@ function App() {
     profilePhoto: "/ucugram-logo.png",
     name: "nombre_ususario",
     posts: 11, // después esto va a ser una lista por ej. 
-    friends: 17 // lista o dict con usuarios amigos
+    friends: 17, // lista o dict con usuarios amigos
+    description: "This is the best app I have ever seen! 😊"
   };
+
+  const friend = {
+    profilePhoto: "/profile_img-by-AI.jpeg",
+    name: "nombre_amigo",
+    posts: 1.861, // después esto va a ser una lista por ej. 
+    friends: 454, // lista o dict con usuarios amigos
+    description: "This is my profile description 😊"
+  }
   return (
     <>
-      <MyProfile
-        user={user}
+      <FriendProfile
+        user={friend}
       />
     </>
   );
