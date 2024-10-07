8161b3cfd74cb0a761ff0dc60b06308f6e80b248
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2024-10-07 17:20:20 -0300

    .

--- ucugram/src/App.jsx
+++ ucugram/src/App.jsx
@@ -8,6 +8,33 @@ import Notifications from "./pages/notifications/notifications";
 import Avatar from "./components/avatar/avatar";
 
 function App() {
+  const notificationsList = [
+    {
+      id: 1,
+      type: "like",
+      user: {
+        profilePhoto: "/profile_img-by-AI.jpeg",
+        name: "nombre_amigo",
+        posts: 1.861, // después esto va a ser una lista por ej. 
+        friends: 454, // lista o dict con usuarios amigos
+        description: "This is my profile description 😊"
+      },
+      time: "2h"
+    },
+    {
+      id: 2,
+      type: "follow",
+      user: {
+        profilePhoto: "/profile_img-by-AI.jpeg",
+        name: "nombre_amigo",
+        posts: 1.861, // después esto va a ser una lista por ej. 
+        friends: 454, // lista o dict con usuarios amigos
+        description: "This is my profile description 😊"
+      },
+      time: "2h"
+    }
+  ];
+
   const user = {
     profilePhoto: "/ucugram-logo.png",
     name: "nombre_ususario",
@@ -25,7 +52,7 @@ function App() {
   }
   return (
     <>
-      <Notifications user={user} />
+      <Notifications user={user} notificationsList={notificationsList}/>
     </>
   );
 }
