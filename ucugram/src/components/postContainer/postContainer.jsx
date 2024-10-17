47cbcaabb691fd8553385467305326d7ae1905d9
PauloinaVidal22 <paulina.vidal@correo.ucu.edu.uy>
2024-10-17 19:45:15 -0300

    getting started

---
+++ ucugram/src/components/postContainer/postContainer.jsx
@@ -0,0 +1,9 @@
+import React,  { useRef, useState } from "react";
+import LikeButton from "ucugram/src/components/likeButton/likeButton.jsx";
+import CommentButton from "../commentButton/commentButton";
+import Avatar from 'ucugram/src/components/avatar/avatar.jsx'
+// import CommentInput from "../commentInput/commentInput";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// faHeart, faComment,
+import {  faShare } from '@fortawesome/free-solid-svg-icons';
+import './postModal.css';
