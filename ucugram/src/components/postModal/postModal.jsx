209bd6dc7f48eb74d6988e2c3237d2234f1a75d8
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2024-10-19 14:16:20 -0300

    responsive post modal

--- ucugram/src/components/postModal/postModal.jsx
+++ ucugram/src/components/postModal/postModal.jsx
@@ -1,4 +1,4 @@
-import React,  { useRef, useState } from "react";
+import React,  { useRef, useState, useEffect } from "react";
 import LikeButton from "ucugram/src/components/likeButton/likeButton.jsx";
 import CommentButton from "../commentButton/commentButton";
 import Avatar from 'ucugram/src/components/avatar/avatar.jsx'
@@ -6,10 +6,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faShare } from '@fortawesome/free-solid-svg-icons';
 import './postModal.css';
 
-
 const PostModal = ({ post, isOpen, onClose }) => {
-  if (!isOpen || !post) return null;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (!isOpen || !post) return null;
 
   return (
     <div className={`modal ${isOpen ? "is-active" : ""}`}>
@@ -19,47 +31,61 @@ const PostModal = ({ post, isOpen, onClose }) => {
           aria-label="close" 
           onClick={onClose} >
       </button>
-
       <div className="modal-content" >
-
-        <div className="modal-card-body">
-          <figure className="pic is-4by3">
+        <div className="modal-card-body p-0">
+          <figure className="image is-square">
             <img src={post.imageUrl} alt={post.description}  />
           </figure>
 
-          <div className="data-container p-4">
-              
-              <div className="post-header is-flex is-justify-content-space-between">
-                <div className="user-info is-flex is-align-items-center">
-                  <Avatar className="user-avatar" user={post.user} ></Avatar>
-                  <h3 className="ml-2" ><strong>{post.user}</strong></h3>
+          {windowWidth > 580 ? (
+            <div className="data-container p-4">
+                <div className="post-header is-flex is-justify-content-space-between">
+                  <div className="user-info is-flex is-align-items-center">
+                    <Avatar className="user-avatar" user={post.user} ></Avatar>
+                    <h3 className="ml-2" ><strong className="has-text-white">{post.user}</strong></h3>
+                  </div>
+                  <p className="post-date">{post.date}</p>
                 </div>
-                <p className="post-date">{post.date}</p>
-              </div>
+                <p className="pic-description mt-3"><strong className="has-text-white">{post.user}</strong> {post.description}</p>
 
-
-              <p className="pic-description mt-3"><strong>{post.user}</strong> {post.description}</p>
-
-              <ul className="comments-section mt-4">
-                {post.comments.map((comment) => (
-                  <li key={comment.id} className="mb-2">
-                  <p><strong>{comment.user}</strong> {comment.text} </p>
-                  </li>
-                  ))}
-              </ul>
-              
-
-              <div className="buttons is-centered mt-4">
-
+                <ul className="comments-section mt-4">
+                  {post.comments.map((comment) => (
+                    <li key={comment.id} className="mb-2">
+                    <p><strong className="has-text-white">{comment.user}</strong> {comment.text} </p>
+                    </li>
+                    ))}
+                </ul>
+                <div className="buttons is-centered mb-1 mt-6">
+                    <LikeButton className="like-section" postId={post.id} initialLikes={post.likes} />
+                    <FontAwesomeIcon className="share-button ml-3" icon={faShare}/>
+                    <CommentButton className="comment-button ml-3" postId={post.id} />
+                </div> 
+            </div>
+          ) : (
+            <div className="data-container p-4">
+                <div className="buttons is-centered mb-5">
                   <LikeButton className="like-section" postId={post.id} initialLikes={post.likes} />
-
                   <FontAwesomeIcon className="share-button ml-3" icon={faShare}/>
-
                   <CommentButton className="comment-button ml-3" postId={post.id} />
+                </div> 
+                <div className="post-header is-flex is-justify-content-space-between">
+                  <div className="user-info is-flex is-align-items-center">
+                    <Avatar className="user-avatar" user={post.user} ></Avatar>
+                    <h3 className="ml-2" ><strong className="has-text-white">{post.user}</strong></h3>
+                  </div>
+                  <p className="post-date">{post.date}</p>
+                </div>
+                <p className="pic-description mt-3"><strong className="has-text-white">{post.user}</strong> {post.description}</p>
 
-              </div> 
-
-          </div>
+                <ul className="comments-section mt-4">
+                  {post.comments.map((comment) => (
+                    <li key={comment.id} className="mb-2">
+                    <p><strong className="has-text-white">{comment.user}</strong> {comment.text} </p>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
