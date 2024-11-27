21bf0e908634411cf3946f3635329ac10a0adc79
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2024-11-26 22:32:27 -0300

    create account form

--- ucugram-mobile/components/SignUpForm.js
+++ ucugram-mobile/components/SignUpForm.js
@@ -1,26 +1,12 @@
-import React, { useRef, useState } from "react";
-import {
-  TouchableOpacity,
-  View,
-  StyleSheet,
-  Image,
-  TextInput,
-  Text,
-} from "react-native";
-import useCreateUser from "../hooks/useCreateUser";
+import React, { useRef } from "react";
+import { TouchableOpacity, View, StyleSheet, Image, TextInput, Text } from "react-native";
 
 function SignUpForm({ goLogin }) {
-  const { postUserAW, createdUser, loading, error } = useCreateUser();
   const emailRef = useRef("");
   const passwordRef = useRef("");
   const userNameRef = useRef("");
   const nameRef = useRef("");
   const lastNameRef = useRef("");
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    username: "",
-  });
 
   const PASSWORD_WARNING = "Password must be at least 10 characters";
 
@@ -28,226 +14,200 @@ function SignUpForm({ goLogin }) {
     goLogin();
   };
 
-  const handleOnChange = (field, value) => {
-    setUser({ ...user, [field]: value });
-  };
-
   const validateEmail = (email) => {
-    const ucuEmailPattern =
-      /^[a-zA-Z0-9._%+-]+@(ucu\.edu\.uy|correo\.ucu\.edu\.uy)$/;
+    const ucuEmailPattern = /^[a-zA-Z0-9._%+-]+@(ucu\.edu\.uy|correo\.ucu\.edu\.uy)$/;
     return ucuEmailPattern.test(email);
   };
 
-  const handleCreateAccountBtn = async () => {
+  const handleCreateAccountBtn = () => {
     const newEmail = emailRef.current.value;
     const newPassword = passwordRef.current.value;
     const newUserName = userNameRef.current.value;
     const newName = nameRef.current.value;
     const newLastName = lastNameRef.current.value;
 
-    if (
-      user.email === "" ||
-      user.password === "" ||
-      user.username === "" ||
-      newName === "" ||
-      newLastName === ""
-    ) {
-      window.alert("Faltan completar datos!!");
-    }
-
-    if (!validateEmail(user.email)) {
-      console.log(user.email);
+    if (!validateEmail(newEmail)) {
       alert("Por favor, ingresa un email válido de la UCU.");
       return;
     }
 
-    if (user.password.length < 10) {
-      window.alert(PASSWORD_WARNING);
-    }
-    try {
-      await postUserAW(user);
-      goLogin();
-    } catch (err) {
-      console.error(err);
-      // manejar el error de otra forma
-      window.alert(
-        "Hubo un problema creando el usuario. Por favor, revisa los datos."
-      );
-    }
+    // more logic here
 
+    goLogin();
   };
 
   return (
-    <View style={styles.content}>
-      <View style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Image
-            source={require("../assets/ucugram-logo.png")}
-            style={styles.logo}
-          />
-          <Image
-            source={require("../assets/ucugram-text-sinFondo.png")}
-            style={styles.isologo}
-          />
-        </View>
-        <View style={styles.userInfoContainer}>
-          <View style={{ width: "48%" }}>
-            <Text aria-label="name" style={styles.label}>
-              Name
-            </Text>
+      <View style={styles.content}>
+        <View style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Image source={require('../assets/ucugram-logo.png')} style={styles.logo} />
+            <Image
+              source={require('../assets/ucugram-text-sinFondo.png')}
+              style={styles.isologo}
+            />
+          </View>
+          <View style={styles.userInfoContainer}>
+            <View style={[styles.field, { width: "48%" }]}>
+              <Text aria-label="name" style={styles.label}>Name</Text>
+              <TextInput
+                style={styles.input}
+                type="text"
+                placeholder="Enter your name"
+                ref={nameRef}
+              ></TextInput>
+            </View>
+            <View style={[styles.field, { width: "48%" }]}>
+              <Text aria-label="last name" style={styles.label}>Last Name</Text>
+              <TextInput
+                style={styles.input}
+                type="text"
+                placeholder="Enter your last name"
+                ref={lastNameRef}
+              ></TextInput>
+            </View>
+          </View>
+          <View style={styles.field}>
+            <Text aria-label="username" style={styles.label}>Username</Text>
             <TextInput
-              style={styles.TextInput}
+              style={styles.input}
               type="text"
-              placeholder="Enter your name"
-              ref={nameRef}
+              placeholder="Choose a username"
+              ref={userNameRef}
             ></TextInput>
           </View>
-          <View style={{ width: "48%" }}>
-            <Text aria-label="last name" style={styles.label}>
-              Last Name
-            </Text>
+          <View style={styles.field}>
+            <Text aria-label="email" style={styles.label}>email</Text>
             <TextInput
-              style={styles.TextInput}
-              type="text"
-              placeholder="Enter your last name"
-              ref={lastNameRef}
+              style={styles.input}
+              type="email"
+              placeholder="Enter your email"
+              ref={emailRef}
             ></TextInput>
           </View>
-        </View>
-        <View style={styles.field}>
-          <Text aria-label="username" style={styles.label}>
-            Username
-          </Text>
-          <TextInput
-            style={styles.TextInput}
-            type="text"
-            placeholder="Choose a username"
-            value={user.username}
-            onChangeText={(text) => handleOnChange("username", text)}
-          ></TextInput>
-        </View>
-        <View style={styles.field}>
-          <Text aria-label="email" style={styles.label}>
-            Email
-          </Text>
-          <TextInput
-            style={styles.TextInput}
-            type="email-address"
-            placeholder="Enter your email"
-            value={user.email}
-            onChangeText={(text) => handleOnChange("email", text)}
-          ></TextInput>
-        </View>
-        <View style={styles.field}>
-          <Text aria-label="password" style={styles.label}>
-            Password
-          </Text>
-          <TextInput
-            style={styles.TextInput}
-            type="password"
-            secureTextEntry
-            placeholder="Enter a password"
-            value={user.password}
-            onChangeText={(text) => handleOnChange("password", text)}
-          ></TextInput>
-        </View>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.createAccountButton}
-            onPress={handleCreateAccountBtn}
-          >
-            <Text
-              aria-label="Create new account"
-              style={styles.createAccountButtonText}
-            >
-              Create new account
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{ marginTop: "10px" }}>
-          <TouchableOpacity
-            style={{ font: "12px arial" }}
-            onPress={handleGoBackBtn}
-          >
-            <Text aria-label="go login" style={styles.goLogin}>
-              {"<"} go login
-            </Text>
-          </TouchableOpacity>
+          <View style={styles.field}>
+            <Text aria-label="password" style={styles.label}>password</Text>
+            <TextInput
+              style={styles.input}
+              type="password"
+              placeholder="Enter a password"
+              title={PASSWORD_WARNING}
+              ref={passwordRef}
+            ></TextInput>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.createAccountButton} onPress={handleCreateAccountBtn}>
+              <Text aria-label="Create new account" style={styles.createAccountButtonText}>Create new account</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ marginTop: "15px" }}>
+            <TouchableOpacity style={{ font: "12px arial" }} onPress={handleGoBackBtn}>
+              <View style={styles.buttonTextContainer}>
+                <Text style={styles.createAccountText}>Already have an account?  </Text>
+                <Text style={styles.hereText}>Login</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  content: {
-    backgroundColor: "rgb(220, 221, 230)",
-    padding: 20,
-    borderRadius: 10,
-    width: "100%",
-    height: "auto",
-    maxHeight: "80%",
-    overflow: "hidden",
+    content: {
+        backgroundColor: 'rgb(220, 221, 230)',
+        padding: 20, 
+        borderRadius: 10,
+        width: '100%',
+        height: 'auto',
+        maxHeight: '80%',
+        overflow: 'hidden',
+    },
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    label: {
+      fontSize: 14,
+      fontWeight: 600,
+      color: "rgb(30, 30, 109)",
+      marginBottom: 5,
+    },
+    input: {
+      width: "100%",
+      marginTop: 3,
+      fontSize: 18,
+    },
+    field: {
+      marginBottom: 20,
+      width: "100%",
+      fontSize: 16,
+      borderBottomColor: "rgb(30, 30, 109)",
+      borderBottomWidth: 1,
+      padding: 3,
+    },
+    userInfoContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      width: '100%',
+      marginTop: 10,
+      paddingTop: 30,
   },
-  container: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  field: {
-    marginBottom: 15,
-    width: "100%",
-  },
-  userInfoContainer: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    width: "100%",
-    marginTop: 10,
-    paddingTop: 50,
-  },
-  createAccountButton: {
-    backgroundColor: "rgb(30, 30, 109)",
-    color: "white",
-    width: "100%",
-    borderRadius: 12,
-    padding: 10,
-    textAlign: "center",
-  },
-  createAccountButtonText: {
-    color: "white",
-    textAlign: "center",
-  },
-  logoContainer: {
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingBottom: 20,
-  },
-  isologo: {
-    width: "90%",
-    height: "10%",
-    paddingBottom: 30,
-    resizeMode: "contain",
-    marginBottom: 30,
-  },
-  logo: {
-    width: "50%",
-    height: "10%",
-    marginBottom: 20,
-    resizeMode: "contain",
-  },
-  goLogin: {
-    color: "rgb(30, 30, 109)",
-    paddingTop: 20,
-  },
+    createAccountButton: {
+        backgroundColor: 'rgb(30, 30, 109)',
+        width: '100%',
+        borderRadius: 12,
+        padding: 10,
+        marginBottom: 15,
+    }, 
+    createAccountButtonText:{
+      color: 'white',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+    logoContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingBottom:20,
+    },
+    isologo: {
+      width: "90%",
+      height: "12%",
+      paddingBottom: 30,
+      resizeMode: "contain",
+    },
+    logo: {
+      width: "55%",
+      height: "11%",
+      marginBottom: 12,
+      resizeMode: "contain",
+    },
+    buttonTextContainer: {
+      flexDirection: "row",
+    },
+    createAccountText: {
+      color: "rgb(30, 30, 109)",
+      fontSize: 16,
+    },
+    hereText: {
+      color: "rgb(30, 30, 109)",
+      fontWeight: "bold",
+      fontSize: 16,
+    },
 });
+
+
+
 
 export default SignUpForm;
